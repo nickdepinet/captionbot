e0eea64e11353aba514c43824dc143238c5dc975
Nick Depinet <depinetnick@gmail.com>
2021-03-20 14:24:42 -0700

    single-speaker functional

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # captionbot
 Discord bot for captioning voice chats
+
+# Setup
+
+1) Register your bot with discord, obtain a bot API key, and invite the bot to your server.
+
+2) Create your configuration file, src/.config.json - this contains your API key and command prefix
+
+    ```
+    {
+        "discord-api-key": "discord-api-key",
+        "prefix": "#!"
+    }
+    ```
+3) Download the vosk voice recognition model unzip, and copy to the root captionbot folder
+    ```
+    wget http://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
+    unzip vosk-model-small-en-us-0.15.zip
+    mv vosk-model-small-en-us-0.15 model
+    ```
+4) Install nodejs 14.16 LTS from https://nodejs.org
+5) Install dependencies with npm install
+    Note: On Windows, there's a problem with the vosk npm bindings, you need to copy the following dlls into your PATH:
+    ```
+    libgcc_s_seh-1.dll
+    libstdc++-6.dll
+    libvosk.dll
+    libwinpthread-1.dll
+    ```
+    These files reside in `captionbot/node_modules/vosk/lib/win-x86_64`
+6) Build and run the bot
+    ```
+    npm start
+    ```
+
+# Usage
+Coming soon.
+
+
--- src/bot.ts
+++ src/bot.ts
@@ -17,7 +17,29 @@ var apiKey = config["discord-api-key"];
 client.login(apiKey);
 
 const model = new Vosk.Model("model");
-const rec = new Vosk.Recognizer(model, 16000);
+const rec = new Vosk.Recognizer(model, 48000.0);
+
+interface RecognizedResult {
+    result: any[];
+    text: string;
+}
+
+async function convert_audio(input: Buffer): Promise<Buffer> {
+    try {
+        // stereo to mono channel
+        const data = new Int16Array(input)
+        const ndata = new Int16Array(data.length/2)
+        for (let i = 0, j = 0; i < data.length; i+=4) {
+            ndata[j++] = data[i]
+            ndata[j++] = data[i+1]
+        }
+        return Buffer.from(ndata);
+    } catch (e) {
+        console.log(e)
+        console.log('convert_audio: ' + e)
+        throw e;
+    }
+}
 
 client.on('message', async message => {
     console.log(message.content);
@@ -26,20 +48,30 @@ client.on('message', async message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift()?.toLowerCase();
     const param = args.shift()?.toLowerCase();
-    console.log(command);
     if (command === 'caption' && message.member?.voice.channel) {
         if (param === 'start') {
             const connection: VoiceConnection = await message.member.voice.channel.join();
-            const audio: Readable = connection.receiver.createStream(message, { mode: 'pcm', end: 'manual' });
-            const end_of_speech = rec.acceptWaveform(audio.read());
-            message.channel.send(message.member.user.username + ": " + rec.partialResult());
+            const audio: Readable = connection.receiver.createStream(message, { mode: 'pcm' });
+            let chunks: any = [];
             audio.on('data', function(chunk) {
-                const end_of_speech = rec.acceptWaveForm(chunk);
-                if (end_of_speech) {
-                    message.channel.send(message.member?.user.username ?? 'Unknown User' + ": " + rec.result());
-                }
+                console.log(`Received ${chunk.length} bytes of data.`);
+                chunks.push(chunk);
             });
-            
+            audio.on('end', async () => {
+                let buffer: Buffer = await convert_audio(Buffer.concat(chunks));
+                let rStream: Readable = new Readable();
+                rStream._read = () => {} // _read is required but you can noop it
+                rStream.push(buffer)
+                rStream.push(null)
+                for await (const data of rStream) {
+                    const end_of_speech = rec.acceptWaveform(data);
+                    if (end_of_speech) {
+                        console.log("partial: " + rec.result());
+                    }
+                }
+                let result: RecognizedResult = JSON.parse(rec.finalResult(rec));
+                message.channel.send(message.member?.user.username + ": " + result.text);
+            });
         } else if (param === 'stop') {
             await message.member.voice.channel.leave();
         }
